72a2b96b4b903f76e1fbdf451690809cf3527881
LeSnake04 <dev.lesnake@posteo.de>
2023-10-21 23:24:22 +0200

    init of new homepage

---
+++ content/src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<head>
+    <?php
+    require "base-head.html"
+    ?>
+</head>
+
+<body>
+    <?php
+    require "header.html"
+    ?>
+    <div class="w-full bg-black h-20">
+        <button class="">test</button>
+    </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    <md-block>
+        # Hello
+    </md-block>
+</body>
